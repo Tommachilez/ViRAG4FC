3c61c8876af4e541ce5d7d07d9660152e3fc3f7e
Tommachilez <michael.nguyenbathong@gmail.com>
2025-12-24 15:28:06 +0700

    refactor: stop script if no trained model detected

--- src/scripts/viranker/visualize.py
+++ src/scripts/viranker/visualize.py
@@ -1,3 +1,4 @@
+import sys
 import json
 import logging
 import os
@@ -229,7 +230,7 @@ def main():
     logging.info(f"--- Evaluating Trained Model: {args.trained_model} ---")
     if not os.path.exists(args.trained_model) and not args.trained_model.startswith("namdp-ptit"):
         logging.warning(f"Trained model path '{args.trained_model}' not found on disk. Comparing against 0s.")
-        trained_results = {k: 0.0 for k in base_results}
+        sys.exits(0)
     else:
         trained_results = evaluate_model(args.trained_model, dev_data, args)
 
